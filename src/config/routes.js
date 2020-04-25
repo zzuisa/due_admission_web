273ead43084b3785d707be8a1e177f1cd056eeed
Frank <572220216@qq.com>
2020-04-25 20:54:28 +0200

    init

---
+++ src/config/routes.js
@@ -0,0 +1,121 @@
+/* eslint-disable */
+
+// 工具
+import util from '@/libs/util.ice';
+// 页面和布局
+import Index from '@/pages/Index';
+import Login from '@/pages/Login'
+import Error404 from '@/pages/Error404';
+import Demo1 from '@/pages/Demo1';
+import Demo2 from '@/pages/Demo2';
+import HeaderAside from '@/layouts/HeaderAside';
+
+// 如果不指定 name 字段，会根据 path 生成 name = page-demo1
+// 转换规则见 util.recursiveRouterConfig 中 path2name 方法
+// meta 字段会和默认值使用 Object.assign 合并
+// 如果不指定 meta.name 的话，name 字段会使用和上面路由 name 一样的取值逻辑
+// 下面两个页面就是对比 你可以分别观察两个页面上显示的路由数据差异
+
+// 变量名 routerConfig 为 iceworks 检测关键字
+// ice 会自动在这个变量下添加路由数据
+// 请不要修改名称
+const routerConfig = [
+  {
+    path: '/',
+    title: 'Home',
+    component: HeaderAside,
+    children: [
+      { path: 'demo1', component: Demo1 },
+      { path: 'demo2', component: Demo2 },
+      // keep the name param
+      { path: '', component: Index, name: 'index' },
+    ],
+  },
+  { path: '*', component: Error404 },
+  { path: '/index', component: Index },
+];
+
+const routerConfig2 = [
+  {
+    path: '/',
+    title: 'Home',
+    component: HeaderAside,
+    children: [
+      { path: 'demo1', component: Demo1,title: 'Home', },
+      // keep the name param
+      { path: '', component: Index, name: 'index' },
+    ],
+  },
+  { path: '*', component: Error404 },
+  { path: '/index', component: Index },
+];
+
+// 不参与菜单显示的
+// ice 不会处理这部分
+// 但是这部分路由也会被注册
+// 处理规则同 routerConfig
+
+const routerConfigMenuLogin = [
+  // 登录
+  
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      auth: false,
+    },
+  },
+
+  // 404
+  {
+    path: '*',
+    component: Error404,
+  },
+];
+
+// 导出全部路由设置
+export default util.recursiveRouterConfig([
+  ...routerConfig,
+  ...routerConfig2,
+  ...routerConfigMenuLogin,
+]);
+
+function getFrameInRoutes() {
+  const frameInRoutes = [];
+  util.recursiveRouterConfig(routerConfig).forEach(e => {
+    const { path, name, hidden, meta, children } = e;
+    if (children) {
+      children.forEach(child => {
+        const { path, name, hidden, meta } = child;
+        frameInRoutes.push({ path, name, hidden, meta });
+      });
+    } else {
+      frameInRoutes.push({ path, name, hidden, meta });
+    }
+  });
+
+  return frameInRoutes;
+}
+
+function getFrameInRoutes2() {
+  const frameInRoutes = [];
+  util.recursiveRouterConfig(routerConfig2).forEach(e => {
+    const { path, name, hidden, meta, children } = e;
+    if (children) {
+      children.forEach(child => {
+        const { path, name, hidden, meta } = child;
+        frameInRoutes.push({ path, name, hidden, meta });
+      });
+    } else {
+      frameInRoutes.push({ path, name, hidden, meta });
+    }
+  });
+
+  return frameInRoutes;
+}
+
+// 导出参与多标签页处理的路由设置
+// 这个数据会在 main.js 中使用
+export const frameInRoutes = getFrameInRoutes();
+export const router2 = getFrameInRoutes2();
---
+++ src/pages/Index/page.vue
@@ -0,0 +1,84 @@
+<template>
+  <d2-container class="page">
+    <div style="width:100%;margin:0 auto;text-align:center">
+      <p>
+        <a href="index.html">
+          <img :src="studying" width="230" height="110" border="0" />
+        </a>
+        <img :src="studium" height="110" />
+        <img :src="zollvereinruhr" height="110" />
+        <img :src="logo" height="110" />
+        <img :src="innenhafen" width="218" height="110" />
+        <img :src="bach" height="110" />
+      </p>
+    </div>
+    <d2-page-cover>
+      <d2-icon-svg class="page__logo" name="d2-admin" />
+      <template slot="footer">
+        <div class="page__btn-group">
+          <span @click="$open('https://github.com/d2-projects')">开源组织</span> |
+          <span @click="$open('https://fairyever.com/d2-admin/doc/zh/')">文档</span> |
+          <span @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版</span> |
+          <span @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰</span> |
+          <span @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金</span>
+          <el-popover :width="172" trigger="hover">
+            <p class="d2-mt-0 d2-mb-10">D2Projects</p>
+            <img src="./image/qr@2x.png" style="width: 172px;" />
+            <span slot="reference">微信公众号</span>
+            <p
+              style="font-size: 12px; margin-top: 0px; margin-bottom: 0px;"
+            >官方公众号，主要推送前端技术类文章、框架资源、学习教程，以及 D2 系列项目更新信息</p>
+          </el-popover>
+        </div>
+        <d2-badge />
+        <d2-help-btn />
+      </template>
+    </d2-page-cover>
+  </d2-container>
+</template>
+
+<script>
+import D2HelpBtn from "./components/d2-help-btn";
+import D2Badge from "./components/d2-badge";
+import D2PageCover from "./components/d2-page-cover";
+export default {
+  components: {
+    D2HelpBtn,
+    D2Badge,
+    D2PageCover
+  },
+  data() {
+    return {
+      studying: "images/studying.jpg",
+      logo: "images/logo_claim_en_72dpi_rgb.jpg",
+      bach: "images/bach-end.jpg",
+      innenhafen:
+        "images/innenhafen_duisburgruhr_tourismusjochen_schlutius300x150_300x150.jpg",
+      zollvereinruhr:
+        "images/zollvereinruhr_tourismusjochen_schlutius300x300_300x300.jpg",
+      studium: "images/studium_foerderung_neu_300x300.jpg",
+      filename: __filename
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+  .page__logo {
+    width: 120px;
+  }
+  .page__btn-group {
+    color: $color-text-placehoder;
+    font-size: 12px;
+    margin-top: 0px;
+    margin-bottom: 20px;
+    span {
+      color: $color-text-sub;
+      &:hover {
+        color: $color-text-main;
+      }
+    }
+  }
+}
+</style>
